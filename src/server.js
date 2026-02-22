ca58d3609c2bacc3864a456ffc60f0912ad9e973
Nugroho <nugrohonug066@gmail.com>
2026-02-22 07:48:22 +0700

    vercel config

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 const app = require('./app');
 const { PORT } = require('./config/env');
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
+// });
+module.exports = app
