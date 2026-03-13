8219d826796c4e54e6ab2e47bfd118fe0107772e
shidqisep <shidathalah@gmail.com>
2026-03-13 10:58:16 +0700

    Crud users sudah selesai

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -8,6 +8,10 @@ const userController = require('./user.controller');
 // Semua route di dalam modul user akan diproteksi oleh auth middleware
 router.use(auth);
 
+// Rute CRUD Profil User
+router.get('/me', userController.getProfile);          // (Read)   GET /api/users/me
+router.put('/me', userController.updateProfile);       // (Update) PUT /api/users/me
+router.delete('/me', userController.deleteAccount);    // (Delete) DELETE /api/users/me
 router.patch('/change-password', userController.changePassword);
 
 module.exports = router;
